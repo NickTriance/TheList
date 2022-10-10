d5823224646bf52e895910e944e5a782f50a2ee1
NickTriance <114621135+NickTriance@users.noreply.github.com>
2022-10-10 12:29:10 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace TheListQuote
+{
+    partial class QuoteGetter
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumberLabel = new System.Windows.Forms.Label();
+            this.QuoteBox = new System.Windows.Forms.TextBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumberLabel
+            // 
+            this.NumberLabel.AutoSize = true;
+            this.NumberLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumberLabel.Location = new System.Drawing.Point(262, 104);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(289, 32);
+            this.NumberLabel.TabIndex = 0;
+            this.NumberLabel.Text = "Click button to get quote.";
+            // 
+            // QuoteBox
+            // 
+            this.QuoteBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.QuoteBox.Location = new System.Drawing.Point(249, 149);
+            this.QuoteBox.Multiline = true;
+            this.QuoteBox.Name = "QuoteBox";
+            this.QuoteBox.ReadOnly = true;
+            this.QuoteBox.Size = new System.Drawing.Size(302, 123);
+            this.QuoteBox.TabIndex = 1;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(343, 289);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(111, 39);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Get a quote!";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 334);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Previous Quote";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // QuoteGetter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.QuoteBox);
+            this.Controls.Add(this.NumberLabel);
+            this.Name = "QuoteGetter";
+            this.Text = "The List: Random Quote";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NumberLabel;
+        private System.Windows.Forms.TextBox QuoteBox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+
+namespace TheListQuote
+{
+    public partial class QuoteGetter : Form
+    {
+        //url for downloading the list
+        private readonly string LIST_URL = "https://docs.google.com/document/d/14Jy3JvdCvajXGiOo7O-oxCec9W4fwZyHBCpiNbJSy3Y/export?format=txt";
+        private readonly string fileName = String.Format(@"{0}\list.txt", Application.StartupPath); //where the list will be saved
+        
+        public QuoteGetter()
+        {
+            InitializeComponent();
+            DownloadList();
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+
+        string[] lines;
+
+        int quoteNumber;
+
+        //This method doesn't do anything but it needs to exist otherwise winforms gets all pissy.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //callback for startbutton
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            getRandomQuote();
+            button1.Visible = true;
+            button2.Visible = true;
+            StartButton.Text = "Random";
+        }
+
+        //method to download list
+        private void DownloadList()
+        {
+            //If we already have the list downloaded, delete it and download it again incase there have been any changes. I know this sucks, but I'm making this for fun so i dont care.
+            if(File.Exists(fileName))
+            {
+                Console.WriteLine($"Found old version of list at {fileName}, deleting and redownloading");
+                
+                try
+                {
+                    File.Delete(fileName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"UWU FUCKY WUCKY WHILE DELETING LIST, IS IT IN USE? {e.ToString()}");
+                }
+            }
+
+            //we'll just download it using WebClient. There are better ways of doing this, but this is the simplest.
+            WebClient client = new WebClient();
+            Uri uri = new Uri(LIST_URL);
+            client.DownloadFile(uri, fileName); //yes, not doing this async causes the program to momentarily hang, but it removes the problem of trying to access the list while it is still downloading
+            InitializeList();
+        }
+
+        private void InitializeList()
+        {
+            if(File.Exists(fileName))
+            {
+                try
+                {
+                    lines = System.IO.File.ReadAllLines(fileName);
+                } catch (Exception e)
+                {
+                    Console.WriteLine($"Error while loading list into memory {e.ToString()}");
+                    DownloadList();
+                }
+            }
+        }
+
+        private void getRandomQuote()
+        {
+            if (lines[0] != null)
+            {
+                Random rand = new Random();
+                quoteNumber = rand.Next(lines.Length);
+                quoteNumber++; //the first line of the text document we are reading isn't actually a quote, so we'll offset by 1 to compensate.
+                
+                setDisplay(quoteNumber);
+            } else
+            {
+                DownloadList();
+                getRandomQuote();
+            }
+        }
+
+        public string getQuote(int num)
+        {
+            if(num == 0)
+            {
+                return getQuote(lines.Length - 1);
+            }
+            if (lines[0] != null)
+            {
+                string returnString = "";
+                int trimAmt = num.ToString().Length;
+                returnString = lines[quoteNumber];
+                returnString = returnString.Remove(0, trimAmt + 1);
+                return returnString;
+            }
+            else
+            {
+                DownloadList();
+                getRandomQuote();
+                return getQuote(num);
+            }
+        }
+
+        private void setDisplay(int _quoteNum)
+        {
+            QuoteBox.Text = getQuote(_quoteNum);
+            NumberLabel.Text = _quoteNum + ".";     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (quoteNumber < lines.Length - 1)
+            {
+                quoteNumber++;
+                setDisplay(quoteNumber);
+            }
+            else
+            {
+                quoteNumber = 1;
+                setDisplay(quoteNumber);
+            }
+        }
+
+        //callback for prev button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (quoteNumber > 1)
+            {
+                quoteNumber--;
+                setDisplay(quoteNumber);
+            } else
+            {
+                quoteNumber = lines.Length - 1;
+                setDisplay(quoteNumber);
+            }
+        }
+    }
+}
